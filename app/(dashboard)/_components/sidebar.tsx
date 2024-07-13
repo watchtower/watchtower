b3b9f6b44c3b23ea01c59084a8889aeba62cdc15
Swargaraj Bhowmik <swargarajbhowmik@gmail.com>
2024-07-13 14:44:13 +0530

    chore: minor sidebar changes and layout fixs

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -14,27 +14,27 @@ export default function Sidebar() {
 
   return (
     <div className="hidden border-r bg-muted/40 md:block select-none">
-      <div className="flex h-full max-h-screen flex-col gap-2">
+      <div className="flex h-full max-h-screen flex-col">
         <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
           <Link href="/" className="flex items-center gap-2 font-semibold">
             <span>WatchTower</span>
           </Link>
         </div>
         <div className="flex-1">
-          <nav className="grid items-start px-2 space-y-1 text-sm font-medium lg:px-4">
+          <nav className="grid items-start text-sm font-medium">
             {menu.map((item) => (
               <Link
                 key={item.label}
                 href={item.href}
                 className={cn(
-                  "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary hover:bg-muted",
+                  "flex items-center gap-3 px-4 pl-4 lg:pl-6 py-3.5 text-muted-foreground transition-all hover:text-primary hover:bg-muted",
                   pathname === item.href ? "bg-muted" : ""
                 )}
               >
                 <item.icon className="h-4 w-4" />
                 {item.label}
                 {item.badge && (
-                  <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                  <Badge className="ml-auto flex w-6 h-6 shrink-0 items-center justify-center rounded-full">
                     1
                   </Badge>
                 )}
--- app/(dashboard)/monitors/page.tsx
+++ app/(dashboard)/monitors/page.tsx
@@ -1,8 +1,24 @@
+import { Button } from "@/components/ui/button";
+
 export default function Monitors() {
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
       <div className="flex items-center">
         <h1 className="text-lg font-semibold md:text-2xl">Monitors</h1>
+      </div>
+      <div
+        className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
+        x-chunk="dashboard-02-chunk-1"
+      >
+        <div className="flex flex-col items-center gap-1 text-center">
+          <h3 className="text-2xl font-bold tracking-tight">
+            You don&apos;t have any monitors
+          </h3>
+          <p className="text-sm text-muted-foreground">
+            Start by adding a new monitor
+          </p>
+          <Button className="mt-4">Add Monitor</Button>
+        </div>
       </div>
     </main>
   );
