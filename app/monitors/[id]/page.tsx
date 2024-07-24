12ac022e94424a8739ad2f1fde1b86717fc3bd7b
james-tindal <10291002+james-tindal@users.noreply.github.com>
2024-07-24 09:53:31 +0100

    fix: timestamp as key for ping display list

--- app/monitors/[id]/page.tsx
+++ app/monitors/[id]/page.tsx
@@ -1,16 +1,15 @@
 import { getPingsForMonitor } from "@/services/monitor";
 
-
 export default async function Monitor(props: any) {
   const id: string = props.params.id
   const pings = await getPingsForMonitor(id)
 
   return (
     <main>
-      {pings.map((ping, index) =>
+      {pings.map(ping =>
         ping.status >= 400 && ping.status < 500
-        ? <div key={index}>green</div>
-        : <div key={index}>red</div>
+        ? <div key={ping.start.getTime()}>green</div>
+        : <div key={ping.start.getTime()}>red</div>
       )}
     </main>
   );
