46645d80e8070a5742ad80e7bc33e112d48f33c4
Swargaraj Bhowmik <swargarajbhowmik@gmail.com>
2024-07-28 17:02:40 +0530

    temp commented old files

--- actions/monitor.ts
+++ actions/monitor.ts
@@ -1,28 +1,28 @@
-"use server";
+// "use server";
 
-import { addMonitor, deleteMonitor } from "@/services/monitor";
-import { revalidatePath } from "next/cache";
+// import { addMonitor, deleteMonitor } from "@/services/monitor";
+// import { revalidatePath } from "next/cache";
 
-export async function onAddMonitor(name: string, url: string) {
-  try {
-    const monitor = await addMonitor(name, url);
-    if (monitor) {
-      revalidatePath(`/monitors`);
-    }
-    return monitor;
-  } catch (error) {
-    throw new Error("Server Error");
-  }
-}
+// export async function onAddMonitor(name: string, url: string) {
+//   try {
+//     const monitor = await addMonitor(name, url);
+//     if (monitor) {
+//       revalidatePath(`/monitors`);
+//     }
+//     return monitor;
+//   } catch (error) {
+//     throw new Error("Server Error");
+//   }
+// }
 
-export async function onDeleteMonitor(id: string) {
-  try {
-    const monitor = await deleteMonitor(id);
-    if (monitor) {
-      revalidatePath(`/monitors`);
-    }
-    return monitor;
-  } catch (error) {
-    throw new Error("Server Error");
-  }
-}
+// export async function onDeleteMonitor(id: string) {
+//   try {
+//     const monitor = await deleteMonitor(id);
+//     if (monitor) {
+//       revalidatePath(`/monitors`);
+//     }
+//     return monitor;
+//   } catch (error) {
+//     throw new Error("Server Error");
+//   }
+// }
--- app/services/_components/service.tsx
+++ app/services/_components/service.tsx
@@ -2,13 +2,13 @@ import { Card } from '@/components/ui/card';
 
 import Link from 'next/link';
 
-import type { Monitors } from '@prisma/client';
+import type { Service } from '@prisma/client';
 
-export default function MonitorCard({ monitor }: { monitor: Monitors }) {
+export default function MonitorCard({ service }: { service: Service }) {
   return (
-    <Link href={`/services/view/${monitor.id}`}>
+    <Link href={`/services/view/${service.id}`}>
       <Card className="flex flex-col space-y-1.5 p-6 rounded-lg bg-secondary border-muted hover:border-primary/60 transition-all cursor-pointer">
-          <span className="text-md font-medium text-primary">{monitor.name}</span>
+        <span className="text-md font-medium text-primary">{service.name}</span>
       </Card>
     </Link>
   );
