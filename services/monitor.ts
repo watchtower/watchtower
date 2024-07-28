c5000cc598b6f279e327d9704ffaec36d024a142
Swargaraj Bhowmik <swargarajbhowmik@gmail.com>
2024-07-28 18:35:42 +0530

    update db schema (#23)
    * update schema
    
    * temp commented old files

--- services/monitor.ts
+++ services/monitor.ts
@@ -1,56 +1,56 @@
-'use server';
+// 'use server';
 
-import { db } from '@/lib/db';
+// import { db } from '@/lib/db';
 
-export async function addMonitor(name: string, url: string) {
-  const monitor = await db.monitors.create({
-    data: {
-      name,
-      url,
-    },
-  });
-  return monitor;
-}
+// export async function addMonitor(name: string, url: string) {
+//   const monitor = await db.monitors.create({
+//     data: {
+//       name,
+//       url,
+//     },
+//   });
+//   return monitor;
+// }
 
-export async function deleteMonitor(id: string) {
-  const monitor = await db.monitors.delete({
-    where: {
-      id,
-    },
-  });
-  return monitor;
-}
+// export async function deleteMonitor(id: string) {
+//   const monitor = await db.monitors.delete({
+//     where: {
+//       id,
+//     },
+//   });
+//   return monitor;
+// }
 
-export async function getMonitors() {
-  const monitors = await db.monitors.findMany({
-    orderBy: {
-      updatedAt: 'desc',
-    },
-  });
-  return monitors;
-}
+// export async function getMonitors() {
+//   const monitors = await db.monitors.findMany({
+//     orderBy: {
+//       updatedAt: 'desc',
+//     },
+//   });
+//   return monitors;
+// }
 
-export async function getMonitor(id: string) {
-  const monitor = await db.monitors.findUnique({
-    where: {
-      id,
-    },
-  });
-  return monitor;
-}
+// export async function getMonitor(id: string) {
+//   const monitor = await db.monitors.findUnique({
+//     where: {
+//       id,
+//     },
+//   });
+//   return monitor;
+// }
 
-export async function updateMonitor(id: string, name: string, url: string) {
-  const monitor = await db.monitors.update({
-    where: {
-      id,
-    },
-    data: {
-      name,
-      url,
-    },
-  });
-  return monitor;
-}
+// export async function updateMonitor(id: string, name: string, url: string) {
+//   const monitor = await db.monitors.update({
+//     where: {
+//       id,
+//     },
+//     data: {
+//       name,
+//       url,
+//     },
+//   });
+//   return monitor;
+// }
 
 // export async function getPingsForMonitor(monitorId: string) {
 //   const pings = await db.ping.findMany({
